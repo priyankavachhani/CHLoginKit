592b025c95b7b9a1921cfaef496e2170c021f676
priyankavachhani@callhippo.com <priyankavachhani@callhippo.com>
2021-01-25 19:47:10 +0530

    abc

---
+++ Login.swift
@@ -0,0 +1,13 @@
+//
+//  Login.swift
+//  CHLoginKit
+//
+//  Created by Apple on 25/01/21.
+//
+
+import Foundation
+
+func loginAdminRequest(_ request: LoginRequest) -> DataRequest {
+    print("PARAMETER_ADMIN = \(request.toJSON())")
+    return RestRequest.getPostRequest("/index.php/api/users/adminLogin", request.toJSON())
+}
